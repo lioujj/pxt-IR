0fdd12fdb74d46666f94e3de2b74bdeddc3fca96
lioujj <34760803+lioujj@users.noreply.github.com>
2018-11-08 10:18:44 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# pxt-IR
+# InfraRed
+
+這是專為micro:bit提供的紅外線積木\
+This is the infrared blocks for microbit
+
+## Author
+Liou Zheng-Ji\
+劉正吉\
+[my page, https://sites.google.com/jes.mlc.edu.tw/ljj/](https://sites.google.com/jes.mlc.edu.tw/ljj/)
+
+## License
+
+* MIT
+
+## Supported targets
+
+* for PXT/microbit
+(The metadata above is needed for package search.)
+
+```package
+IR=github:lioujj/pxt-IR
+```
---
+++ main.ts
@@ -0,0 +1,263 @@
+//% weight=0 color=#87bc4b icon="\uf1eb" block="InfraRed"
+namespace IR {
+    export enum encodingType {
+        //% block="NEC"
+        NEC
+    }
+    let tempHandler: Action;
+    let irLed = AnalogPin.P16;
+    const pwmPeriod = 26;
+    pins.analogWritePin(irLed, 0);
+    pins.analogSetPeriod(irLed, pwmPeriod);
+    let send_init = false;
+    let rec_init = false;
+    let arr: number[] = []
+    let received = false
+    let first = true
+    let rec_Type = ""
+    let messageStr = ""
+    let recPin = DigitalPin.P8
+    arr = []
+
+    function transmitBit(highTime: number, lowTime: number): void {
+        pins.analogWritePin(irLed, 512);
+        control.waitMicros(highTime);
+        pins.analogWritePin(irLed, 0);
+        control.waitMicros(lowTime);
+    }
+
+    /**
+     *  set the infrared LED pin.
+     */
+    //% blockId=setIR_pin block="set IR LED pin: %myPin" blockExternalInputs=false
+    //% weight=90 blockGap=10
+    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
+    //% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
+    export function setIR_pin(myPin: AnalogPin) {
+        irLed = myPin;
+        pins.analogWritePin(irLed, 0);
+        pins.analogSetPeriod(irLed, pwmPeriod);
+        send_init = true;
+    }
+
+    /**
+     *  set the IR receiver pin.
+     */
+    //% blockId=setREC_pin block="set IR receiver pin: %myPin" blockExternalInputs=false
+    //% weight=85 blockGap=10
+    //% myPin.fieldEditor="gridpicker" myPin.fieldOptions.columns=4
+    //% myPin.fieldOptions.tooltips="false" myPin.fieldOptions.width="300"
+    export function setREC_pin(myPin: DigitalPin) {
+        recPin = myPin;
+        pins.setEvents(recPin, PinEventType.Pulse)
+        pins.setPull(recPin, PinPullMode.PullUp)
+        pins.onPulsed(recPin, PulseValue.Low, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        pins.onPulsed(recPin, PulseValue.High, function () {
+            arr.push(input.runningTimeMicros())
+        })
+        control.onEvent(recPin, DAL.MICROBIT_PIN_EVENT_ON_TOUCH, tempHandler);
+        rec_init = true;
+    }
+
+    /**
+     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
+     */
+    //% blockId=sendMyMessage1 block="send message: %msg| ,%times| times, encoding type:%myType"
+    //% weight=80 blockGap=10
+    export function sendMyMessage1(msg: string, times: number, myType: encodingType): void {
+        if (send_init) {
+            //control.inBackground(() => {
+            sendMessage(convertHexStrToNum(msg), times, myType);
+            //})
+        }
+    }
+    /**
+     * send message from IR LED. You must set the message encoding type, send how many times, and the message.
+     */
+    //% blockId=sendMyMessage2 block="send message: %msg| ,%times| times, encoding type:%myType"
+    //% weight=75 blockGap=10
+    export function sendMyMessage2(msg: string, times: number, myType: string): void {
+        if (send_init) {
+            if (myType == "NEC") {
+                sendMessage(convertHexStrToNum(msg), times, encodingType.NEC);
+            }
+        }
+    }
+
+
+    function encode(myCode: number, bits: number, trueHigh: number, trueLow: number, falseHigh: number, falseLow: number): void {
+        const MESSAGE_BITS = bits;
+        for (let mask = 1 << (MESSAGE_BITS - 1); mask > 0; mask >>= 1) {
+            if (myCode & mask) {
+                transmitBit(trueHigh, trueLow);
+            } else {
+                transmitBit(falseHigh, falseLow);
+            }
+        }
+    }
+
+    function sendNEC(message: number, times: number): void {
+        const enum NEC {
+            startHigh = 9000,
+            startLow = 4500,
+            stopHigh = 560,
+            stopLow = 0,
+            trueHigh = 560,
+            trueLow = 1690,
+            falseHigh = 560,
+            falseLow = 560,
+            interval = 110000
+        }
+        //let address = Math.idiv(message, 0x010000)
+        let address = message >> 16;
+        let command = message % 0x010000;
+        const MESSAGE_BITS = 16;
+        let startTime = 0;
+        let betweenTime = 0;
+        for (let sendCount = 0; sendCount < times; sendCount++) {
+            startTime = input.runningTimeMicros();
+            transmitBit(NEC.startHigh, NEC.startLow);
+            encode(address, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
+            encode(command, 16, NEC.trueHigh, NEC.trueLow, NEC.falseHigh, NEC.falseLow);
+            transmitBit(NEC.stopHigh, NEC.stopLow);
+            betweenTime = input.runningTimeMicros() - startTime
+            if (times > 0)
+                control.waitMicros(NEC.interval - betweenTime);
+        }
+    }
+
+    export function sendMessage(message: number, times: number, myType: encodingType): void {
+        switch (myType) {
+            case encodingType.NEC: sendNEC(message, times);
+            default: sendNEC(message, times);
+        }
+    }
+
+    function convertHexStrToNum(myMsg: string): number {
+        let myNum = 0
+        for (let i = 0; i < myMsg.length; i++) {
+            if ((myMsg.charCodeAt(i) > 47) && (myMsg.charCodeAt(i) < 58)) {
+                myNum += (myMsg.charCodeAt(i) - 48) * (16 ** (myMsg.length - 1 - i))
+            } else if ((myMsg.charCodeAt(i) > 96) && (myMsg.charCodeAt(i) < 103)) {
+                myNum += (myMsg.charCodeAt(i) - 87) * (16 ** (myMsg.length - 1 - i))
+            } else if ((myMsg.charCodeAt(i) > 64) && (myMsg.charCodeAt(i) < 71)) {
+                myNum += (myMsg.charCodeAt(i) - 55) * (16 ** (myMsg.length - 1 - i))
+            } else {
+                myNum = 0
+                break
+            }
+        }
+        return myNum
+    }
+
+    //------------------receiver-------------
+
+    function resetReceiver() {
+        arr = []
+        received = false
+    }
+
+    control.inBackground(function () {
+        basic.forever(function () {
+            if ((!received) && (rec_init)) {
+                if (arr.length > 30) {
+                    if ((input.runningTimeMicros() - arr[arr.length - 1]) > 120000) {
+                        if (first) {
+                            resetReceiver()
+                            first = false
+                        } else {
+                            received = true
+                            decodeIR();
+                            //tempHandler();
+                            //control.raiseEvent(recPin, DAL.MICROBIT_PIN_EVENT_ON_TOUCH);
+                            //resetReceiver()
+                        }
+                    }
+                }
+            }
+        })
+    })
+
+    function decodeIR() {
+        let addr = 0
+        let command = 0
+        messageStr = ""
+        rec_Type = ""
+        for (let i = 0; i <= arr.length - 1 - 1; i++) {
+            arr[i] = arr[i + 1] - arr[i]
+        }
+        if ((arr[0] > 9000 && arr[0] < 10000) && (arr[1] > 4000 && arr[1] < 5000)) {
+            rec_Type = "NEC"
+            arr.removeAt(1)
+            arr.removeAt(0)
+            addr = pulseToDigit(0, 15, 1600)
+            command = pulseToDigit(16, 31, 1600)
+            messageStr = convertNumToHexStr(addr, 4) + convertNumToHexStr(command, 4)
+            arr = [];
+            tempHandler();
+        }
+        resetReceiver();
+    }
+
+    function pulseToDigit(beginBit: number, endBit: number, duration: number): number {
+        let myNum = 0
+        for (let i = beginBit; i <= endBit; i++) {
+            myNum <<= 1
+            if ((arr[i * 2] + arr[i * 2 + 1]) < duration) {
+                myNum += 0
+            } else {
+                myNum += 1
+            }
+        }
+        return myNum
+    }
+
+    function convertNumToHexStr(myNum: number, digits: number): string {
+        let tempDiv = 0
+        let tempMod = 0
+        let myStr = ""
+        tempDiv = myNum
+        while (tempDiv > 0) {
+            tempMod = tempDiv % 16
+            if (tempMod > 9) {
+                myStr = String.fromCharCode(tempMod - 10 + 97) + myStr
+            } else {
+                myStr = tempMod + myStr
+            }
+            tempDiv = Math.idiv(tempDiv, 16)
+        }
+        while (myStr.length != digits) {
+            myStr = "0" + myStr
+        }
+        return myStr
+    }
+
+    /**
+     * Do something when a receive IR
+     */
+    //% blockId=onReceivedIR block="on IR message received" blockInlineInputs=true
+    //% weight=70 blockGap=10
+    export function onReceivedIR(handler: Action): void {
+        tempHandler = handler
+    }
+
+    /**
+     * return the encoding type of the received IR 
+     */
+    //% blockId=getRecType block="the received IR encoding type"
+    //% weight=60 blockGap=10
+    export function getRecType(): string {
+        return rec_Type
+    }
+
+    /**
+     * return the message of the received IR 
+     */
+    //% blockId=getMessage block="the received IR message"
+    //% weight=60 blockGap=10
+    export function getMessage(): string {
+        return messageStr
+    }
+}
